bff6a2f41ffeb75b53143a88e313a3b6508882b0
yack_domnicky <yack_domnicky@mail.ru>
2020-06-13 02:26:48 +0300

    Refactoring code, rename some fuctions

--- model.py
+++ model.py
@@ -1,18 +1,12 @@
-# model parameters:
 from functools import reduce
-
-
-import transport_graph as tg
-
+import torch
 from torch import optim
 
-import torch
-
-import oracles
-import dual_func_calculator as dfc
-from prox_h import ProxH
-
 from data_reader.data_reader import *
+import dual_func_calculator as dfc
+import oracles
+import transport_graph as tg
+
 
 class Model:
     def __init__(self, transport_graph, total_od_flow=None, mu=0.25, rho=0.15):
@@ -21,21 +15,20 @@ class Model:
         self.total_od_flow = total_od_flow
         self.mu = mu
         self.rho = rho
-        self.t = torch.tensor(np.array(transport_graph.graph[['Free Flow Time']], dtype='float64').flatten(), requires_grad=True)
+        self.t = torch.tensor(self.graph.freeflow_times, requires_grad=True)
 
         self.phi_big_oracle = oracles.PhiBigOracle(self.graph, self.graph_correspondences)
         self.primal_dual_calculator = dfc.PrimalDualCalculator(self.phi_big_oracle,
-                                                          self.graph.freeflow_times, self.graph.capacities,
-                                                          mu = self.mu, rho = self.rho)
+                                                               self.graph.freeflow_times,
+                                                               self.graph.capacities,
+                                                               mu=self.mu,
+                                                               rho=self.rho)
 
-    def loss_phi(self):
-        return self.primal_dual_calculator.dual_func_value(self.t)
+    def loss_phi_big(self):
+        return self.primal_dual_calculator.phi_big_value(self.t)
 
     def loss_h(self):
         return self.primal_dual_calculator.h_func(self.t)
-
-    def new_loss(self):
-        return self.primal_dual_calculator.new_dual_func_value(self.t)
 
     def get_flows(self, t):
         if len(self.graph_correspondences) == 0:
@@ -44,9 +37,9 @@ class Model:
         base_v = next(iter(self.graph_correspondences))
 
         in_edges = list(map(lambda x: self.graph.pred_to_edge[x[1]][x[0]], self.graph.in_edges(
-            base_v - 1)))  # list(map(lambda x: x[0], phi_big_oracle.graph.in_edges(v - 1)))
+            base_v - 1)))
         out_edges = list(map(lambda x: self.graph.pred_to_edge[x[1]][x[0]], self.graph.out_edges(
-            base_v - 1)))  # list(map(lambda x: x[1], phi_big_oracle.graph.out_edges(v - 1)))
+            base_v - 1)))
 
         in_edges_sum = flows[in_edges].sum()
         out_edges_sum = flows[out_edges].sum()
@@ -57,16 +50,15 @@ class Model:
                               [self.graph_correspondences[base_v][i] for i in self.graph_correspondences[base_v]])
 
         coef_t = (in_edges_sum - out_edges_sum) / (in_flows_sum - out_lows_sum)
-        #print("asd", coef_t)
         flows = flows / coef_t
         return flows
 
     def check_graph(self, flows):
         for v in self.graph_correspondences.keys():
             in_edges = list(map(lambda x: self.graph.pred_to_edge[x[1]][x[0]], self.graph.in_edges(
-                v - 1)))  # list(map(lambda x: x[0], phi_big_oracle.graph.in_edges(v - 1)))
+                v - 1)))
             out_edges = list(map(lambda x: self.graph.pred_to_edge[x[1]][x[0]], self.graph.out_edges(
-                v - 1)))  # list(map(lambda x: x[1], phi_big_oracle.graph.out_edges(v - 1)))
+                v - 1)))
 
             in_edges_sum = flows[in_edges].sum()
             out_edges_sum = flows[out_edges].sum()
@@ -84,7 +76,7 @@ class Model:
         grad_sum = None
         for i in range(num_iters):
             optimizer.zero_grad()
-            loss = self.loss_phi()
+            loss = self.loss_phi_big()
             loss.backward()
             if grad_sum is None:
                 grad_sum = -self.t.grad
@@ -93,18 +85,10 @@ class Model:
             loss = self.loss_h()
             loss.backward()
             optimizer.step()
-            # print("new", loss)
-            # print("old", self.loss())
-            # print(self.t.sum())
             print(self.primal_dual_calculator.primal_func_value(self.get_flows(grad_sum)))
 
         flows = self.get_flows(grad_sum)
-        #print(flows - self.t.grad)
-      #  print(len(self.t.grad), self.t.grad)
 
-
-        # flows = self.get_flows()
-        # print(flows)
         #self.check_graph(flows)
 
 
--- oracles.py
+++ oracles.py
@@ -1,37 +1,6 @@
-#import multiprocessing as mp
-from collections import defaultdict
-#from scipy.misc import logsumexp
 import numpy as np
-import time
-from numba import njit
 import torch
-
-
-@njit
-def get_tree_order(nodes_number, targets, pred_arr):
-    #get nodes visiting order for flow calculation
-    visited = np.zeros(nodes_number, dtype = np.bool_)
-    sorted_vertices = [0] * 0
-    for vertex in targets:
-        temp = []
-        while (not visited[vertex]):
-            visited[vertex] = True
-            if pred_arr[vertex] != vertex:
-                temp.append(vertex)
-                vertex = pred_arr[vertex]
-        sorted_vertices[0:0] = temp
-    return sorted_vertices
-
-def get_flows(nodes_number, edges_number, targets, target_flows, pred_arr, pred_edges, sorted_vertices):  
-    flows = np.zeros(edges_number, dtype = np.float_)
-    vertex_flows = np.zeros(nodes_number, dtype = np.float_)
-    vertex_flows[targets] = target_flows
-    for i, vertex in enumerate(sorted_vertices):
-        edge = pred_edges[i]
-        flows[edge] = vertex_flows[vertex]
-        pred = pred_arr[vertex]
-        vertex_flows[pred] += vertex_flows[vertex]
-    return flows
+import time
 
 
 class BaseOracle(object):
@@ -44,103 +13,44 @@ class BaseOracle(object):
         """
         raise NotImplementedError('Func oracle is not implemented.')
 
-    def grad(self, x):
-        """
-        Computes the gradient at point x.
-        """
-        raise NotImplementedError('Grad oracle is not implemented.')
 
 class AutomaticOracle(BaseOracle):
     """
     Oracle for automatic calculations of function kGamma * \Psi (t)
     kGamma -> +0
     """
-
     def __init__(self, source, graph, source_correspondences):
         self.graph = graph
         self.source_index = source - 1
-
-        self.corr_targets = np.array(list(source_correspondences.keys()), dtype = 'int64') - 1
+        self.corr_targets = np.array(list(source_correspondences.keys()), dtype='int64') - 1
         self.corr_values = torch.from_numpy(np.array(list(source_correspondences.values())))
-        self.flows = None
         self.distances = None
 
     def func(self, t_parameter):
         self.update_shortest_paths(t_parameter)
         return - torch.matmul(self.distances, self.corr_values)
 
-    #correct answer if func calculated flows!
-    def grad(self, t_parameter):
-        sorted_vertices = get_tree_order(self.graph.nodes_number, self.corr_targets, self.pred_map)
-        pred_edges = [self.graph.pred_to_edge[vertex][self.pred_map[vertex]] for vertex in sorted_vertices]
-        flows = get_flows(self.graph.nodes_number, self.graph.links_number, 
-                          self.corr_targets, self.corr_values, self.pred_map, pred_edges, sorted_vertices)
-        return - flows
     def update_shortest_paths(self, t_parameter):
-        self.distances, self.pred_map = self.graph.shortest_distances(self.source_index, self.corr_targets, t_parameter)
-
-    def new_func(self, t_parameter):
-        val = None
-        for i in range(self.corr_targets.shape[0]):
-            path_tensor = self.graph.get_path_tensor(self.source_index, self.corr_targets[i])
-            v = self.corr_values[i] * torch.log(torch.exp(-torch.matmul(path_tensor, t_parameter)).sum())
-            if val is None:
-                val = v
-            else:
-                val += v
-        return val
+        self.distances, _ = self.graph.shortest_distances(self.source_index, self.corr_targets, t_parameter)
 
 
 class PhiBigOracle(BaseOracle):
-    def __init__(self, graph, correspondences, processes_number = None):
+    def __init__(self, graph, correspondences):
         self.graph = graph
         self.correspondences = correspondences
-        if processes_number:
-            self.processes_number = processes_number
-        else:
-            self.processes_number = len(correspondences)
-        self.t_current = None
-        self.func_current = None
-        self.grad_current = None
         self.auto_oracles = []
         for source, source_correspondences in self.correspondences.items():
             self.auto_oracles.append(AutomaticOracle(source, self.graph, source_correspondences))
-        self.time = 0.0
-
-    def _reset(self, t_parameter):
-        #print('Start reset')
-        tic = time.time()
-        self.t_current = t_parameter
-        self.func_current = None
-        for auto_oracle in self.auto_oracles:
-            v = auto_oracle.func(self.t_current)
-            if self.func_current is None:
-                self.func_current = v
-            else:
-                self.func_current += v
-        self.time += time.time() - tic
-        #print('Stop reset')
+        self.work_time = 0.0
 
     def func(self, t_parameter):
-        # if self.t_current is None or torch.any(self.t_current != t_parameter):
-        self._reset(t_parameter)
-        return self.func_current
-
-    def grad(self, t_parameter):
         tic = time.time()
-        self.t_current = t_parameter
-        self.grad_current = np.zeros(self.graph.links_number)
+        result = None
         for auto_oracle in self.auto_oracles:
-            self.grad_current += auto_oracle.grad(self.t_current)
-        self.time += time.time() - tic
-        return self.grad_current
-
-    def new_func(self, t_parameter):
-        value = None
-        for auto_oracle in self.auto_oracles:
-            v = auto_oracle.new_func(t_parameter)
-            if value is None:
-                value = v
+            v = auto_oracle.func(t_parameter)
+            if result is None:
+                result = v
             else:
-                value += v
-        return value
+                result += v
+        self.work_time += time.time() - tic
+        return result
